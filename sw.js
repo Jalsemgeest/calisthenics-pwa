c17772c273f6161560e9d1ca17e7bc7d1ba19b84
Jake Alsemgeest <alsemgeester@gmail.com>
2026-05-03 21:46:28 -0400

    Fixing PWA

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'move-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'move-v2';
+const ASSETS = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
